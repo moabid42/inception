d19114a891bf33c7fa0dc39dcdd39c0fd4c3f094
moabid42 <moabid@student.42heilbronn.de>
2023-01-19 22:26:25 +0100

    Eval

---
+++ srcs/nginx/tools/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sed -ie s/'$DOMAIN_NAME'/$DOMAIN_NAME/g /etc/nginx/sites-available/wordpress.conf
+
+openssl req -x509 -nodes -days 365 -subj "/C=DE/ST=BW/L=Heilbronn/O=42Heilbronn/CN=moabid" \
+	-newkey rsa:2048 -keyout /etc/ssl/nginx-selfsigned.key -out /etc/ssl/nginx-selfsigned.crt;
+
+# Note: You should disable gzip for SSL traffic.
+# See: https://bugs.debian.org/773332
+
+
+unlink /etc/nginx/sites-enabled/default
+
+ln -s /etc/nginx/sites-available/wordpress.conf /etc/nginx/sites-enabled/
+
+# small changes to nginx.conf
+sed -ie 's/gzip on;/gzip off;/g' /etc/nginx/nginx.conf
+
+exec "$@"
